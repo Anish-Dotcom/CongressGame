45ed8acf2912df6eed0788564432934c5db071ae
ayanmas <460146@bsd48.org>
2023-09-13 16:29:38 -0700

    more code. you can now drag them using right click and the papers spawn randomly positions

--- Assets/scripts/papercode.cs
+++ Assets/scripts/papercode.cs
@@ -9,13 +9,18 @@ public class papercode : MonoBehaviour
     private Vector3 pos;
     private Quaternion rot;
     private SpriteRenderer spriteRenderer;
+    private bool isRightMouseDown = false;
     // Start is called before the first frame update
     void Start()
     {
         paperpickedup = false;
 
         float randomRot = Random.Range(0, 360);
+        float randomPosx = Random.Range(-8, 8);
+        float randomPosy = Random.Range(-4, 4);
 
+        Vector3 randomPos = new Vector3(randomPosx, randomPosy, 0f);
+        transform.position = randomPos;
         transform.rotation = Quaternion.Euler(0f, 0f, randomRot);
         pos = transform.position;
         rot = transform.rotation;
@@ -38,6 +43,34 @@ public class papercode : MonoBehaviour
                     paperpickedup = true;
                 }
             }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            isRightMouseDown = true;
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            isRightMouseDown = false;
+        }
+
+        if (isRightMouseDown)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+            if (hit.collider != null && paperpickedup == false)
+            {
+                if (hit.collider.gameObject == gameObject)
+                {
+                    Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    mousePosition.z = 0;
+                    transform.position = Vector3.MoveTowards(transform.position, mousePosition, Time.deltaTime * 1000);
+                    pos = transform.position;
+                    rot = transform.rotation;
+                }
+            }
+            
         }
 
         if (Input.GetKeyDown(KeyCode.Escape))
---
+++ Assets/scripts/paperpickedupinstance.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paperpickedupinstance : MonoBehaviour
+{
+    public bool paperpickedup;
+    public static Instance instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
