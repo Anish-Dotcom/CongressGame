97d2d91c94397c1ce56516e99a82df7b90974c81
bradyRozendaal <144848905+bradyRozendaal@users.noreply.github.com>
2023-09-14 08:52:44 -0700

    Main Menu Buttons Added

---
+++ Assets/sceneManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour
+{
+    public void loadMainTable() //Use this for start in main menu at the moment, change for an intro/office later
+    {
+        SceneManager.LoadScene("Table");
+    }
+    public void loadSettings() 
+    {
+        SceneManager.LoadScene("Settings");
+    }
+    public void quitGame() 
+    {
+        Application.Quit();
+    }
+}
