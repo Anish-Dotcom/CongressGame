711e33b1f6700edfeb6c1760d60015c90591e621
Anish-Dotcom <anishrao210@gmail.com>
2023-09-14 19:09:39 -0700

    push

--- Assets/scripts/Stamp.cs
+++ Assets/scripts/Stamp.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class Stamp : MonoBehaviour
 {
+    public float scaleFactor = 5f;
+    public Vector3 originalScale;
+    private Vector3 pos;
+    private Quaternion rot;
+    private SpriteRenderer spriteRenderer;
+    private bool isRightMouseDown = false;
+    public float maxPaperFollowSpeed = 20;
+    public float smoothTime = 0.5f;
+    public bool slowToStop = false;
+    private Vector2 mousePositionSlow;
+    private RaycastHit2D hit;
+
+    Vector2 currentVelocity;
     // Start is called before the first frame update
     void Start()
     {
-        
+        originalScale = transform.localScale;
+        paperpickedupinstance.instance.paperpickedup = false;
+
+        float randomRot = Random.Range(0, 360);
+        float randomPosx = Random.Range(-8, 8);
+        float randomPosy = Random.Range(-4, 4);
+
+        Vector3 randomPos = new Vector3(randomPosx, randomPosy, 0f);
+        transform.position = randomPos;
+        transform.rotation = Quaternion.Euler(0f, 0f, randomRot);
+        pos = transform.position;
+        rot = transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            slowToStop = false;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            hit = Physics2D.Raycast(ray.origin, ray.direction);
+            if (!paperpickedupinstance.instance.paperpickedup && hit.collider != null)
+            {
+                if (hit.collider.gameObject == gameObject)
+                {
+                    isRightMouseDown = true;
+                    transform.localScale = transform.localScale * 1.05f;
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isRightMouseDown = false;
+            if (hit.collider.gameObject == gameObject)
+            {
+                slowToStop = true;
+                transform.localScale = originalScale;
+            }
+            mousePositionSlow = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        if (isRightMouseDown)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = Vector2.SmoothDamp(transform.position, mousePosition, ref currentVelocity, smoothTime, maxPaperFollowSpeed);
+            pos = transform.position;
+            rot = transform.rotation;
+        }
+        if (slowToStop)
+        {
+            transform.position = Vector2.SmoothDamp(transform.position, mousePositionSlow, ref currentVelocity, smoothTime, maxPaperFollowSpeed);
+        }
     }
 }
--- Assets/scripts/StampPickup.cs
+++ Assets/scripts/StampPickup.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class StampPickup : MonoBehaviour
 {
+    public bool stamppickedup;
+    public static StampPickup instance;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
