5353113ac5094c530c9672d780141332a6612a3f
Zypheruss <bradyrozendaal@gmail.com>
2023-09-25 18:09:17 -0700

    Start day
    Made the scene

---
+++ Assets/DayController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayController : MonoBehaviour
+{
+    public static int dayNum = 0;
+    public static GameObject[] paperObjectsForNext;
+    public GameObject[] allPaperObjects;
+    public static int[] approvalPercentageDemographics = new int[7];//The 1%, Middle class, Impoverished, Progressive, Conservative, Federalist, Anti-Federalist
+
+    void Start()
+    {
+        Instantiate(allPaperObjects[0], new Vector3(0f, 0f, 0f), Quaternion.identity);//tutorial/intro paper
+    }
+    void Update()
+    {
+        
+    }
+    public void NewDay() 
+    {
+        for (int i = 0; i < approvalPercentageDemographics.Length; i++) 
+        {
+            if (approvalPercentageDemographics[i] > 100) 
+            {
+                approvalPercentageDemographics[i] = 100;
+            }
+        }
+        for (int i = 0; i < paperObjectsForNext.Length; i++) 
+        {
+            Instantiate (paperObjectsForNext[i], new Vector3(0f, 0f, 0f), Quaternion.identity);
+        }
+    }
+}
