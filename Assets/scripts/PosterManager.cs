c1300deb4a467bd131f2df1e4053c17921668cc4
ayanmas <460146@bsd48.org>
2023-10-22 18:48:33 -0700

    slogan system working. just missing motifs

--- Assets/scripts/PosterManager.cs
+++ Assets/scripts/PosterManager.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class PosterManager : MonoBehaviour
 {
+    public static bool blue = false;
+    public static bool orange = false;
+    public static bool yellow = false;
+    public static bool red = false;
+    public static bool green = false;
+    public static bool gray = false;
+
+    public static bool sloganOpt1 = false;
+    public static bool sloganOpt2 = false;
+    public static bool sloganOpt3 = false;
+    public static bool sloganOpt4 = false;
+    public static bool sloganOpt5 = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,52 +31,81 @@ public class PosterManager : MonoBehaviour
 
     public void ColorBlue() // colors of poster for button
     {
-
+        RemoveAllColors();
+        blue = true;
     }
     public void ColorOrange()
     {
-
+        RemoveAllColors();
+        orange = true;
     }
     public void ColorYellow()
     {
-
+        RemoveAllColors();
+        yellow = true;
     }
     public void ColorRed()
     {
-
+        RemoveAllColors();
+        red = true;
     }
     public void ColorGreen()
     {
-
+        RemoveAllColors();
+        green = true;
     }
     public void ColorGray()
     {
-
+        RemoveAllColors();
+        gray = true;
     }
 
     public void slogan1() // slogan button
     {
-
+        RemoveAllSlogans();
+        sloganOpt1 = true;
     }
     public void slogan2()
     {
-
+        RemoveAllSlogans();
+        sloganOpt2 = true;
     }
     public void slogan3()
     {
-
+        RemoveAllSlogans();
+        sloganOpt3 = true;
     }
     public void slogan4()
     {
-
+        RemoveAllSlogans();
+        sloganOpt4 = true;
     }
     public void slogan5()
     {
-
+        RemoveAllSlogans();
+        sloganOpt5 = true;
     }
 
     public void Motif1() // motif unfinshed button
     {
 
+    }
+
+    public void RemoveAllColors() // reset colors
+    {
+        blue = false;
+        orange = false;
+        yellow = false;
+        red = false;
+        green = false;
+        gray = false;
+    }
+    public void RemoveAllSlogans() //reset slogans
+    {
+        sloganOpt1 = false;
+        sloganOpt2 = false;
+        sloganOpt3 = false;
+        sloganOpt4 = false;
+        sloganOpt5 = false;
     }
 }
