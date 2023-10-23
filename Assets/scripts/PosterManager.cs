2ec395465dc91653c08b3351a5965775a4817a6d
ayanmas <460146@bsd48.org>
2023-10-22 17:32:54 -0700

    couple more poster changes. its not done still but i gtg rn so here ya go.

---
+++ Assets/scripts/PosterManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PosterManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ColorBlue() // colors of poster for button
+    {
+
+    }
+    public void ColorOrange()
+    {
+
+    }
+    public void ColorYellow()
+    {
+
+    }
+    public void ColorRed()
+    {
+
+    }
+    public void ColorGreen()
+    {
+
+    }
+    public void ColorGray()
+    {
+
+    }
+
+    public void slogan1() // slogan button
+    {
+
+    }
+    public void slogan2()
+    {
+
+    }
+    public void slogan3()
+    {
+
+    }
+    public void slogan4()
+    {
+
+    }
+    public void slogan5()
+    {
+
+    }
+
+    public void Motif1() // motif unfinshed button
+    {
+
+    }
+}
