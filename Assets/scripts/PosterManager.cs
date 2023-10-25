43de00efc14dddb3d5aaa51a71a944afacaf0189
ayan masud <460146@bsd48.org>
2023-10-25 14:02:54 -0700

    poster finish i hope

--- Assets/scripts/PosterManager.cs
+++ Assets/scripts/PosterManager.cs
@@ -22,6 +22,8 @@ public class PosterManager : MonoBehaviour
     public static bool motifopt3 = false;
     public static bool motifopt4 = false;
 
+    public int demographicToChange;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,84 +38,115 @@ public class PosterManager : MonoBehaviour
 
     public void ColorBlue() // colors of poster for button
     {
+        demographicToChange = 0;
+        DayController.approvalPercentageDemographics[demographicToChange] += 2;
         RemoveAllColors();
         blue = true;
     }
     public void ColorOrange()
     {
+        demographicToChange = 2;
+        DayController.approvalPercentageDemographics[demographicToChange] += 2;
         RemoveAllColors();
         orange = true;
     }
     public void ColorYellow()
     {
+        demographicToChange = 4;
+        DayController.approvalPercentageDemographics[demographicToChange] += 2;
         RemoveAllColors();
         yellow = true;
     }
     public void ColorRed()
     {
+        demographicToChange = 5;
+        DayController.approvalPercentageDemographics[demographicToChange] += 2;
         RemoveAllColors();
         red = true;
     }
     public void ColorGreen()
     {
+        demographicToChange = 1;
+        DayController.approvalPercentageDemographics[demographicToChange] += 2;
         RemoveAllColors();
         green = true;
     }
     public void ColorGray()
     {
+        demographicToChange = 2;
+        DayController.approvalPercentageDemographics[demographicToChange] += 2;
         RemoveAllColors();
         gray = true;
     }
 
     public void slogan1() // slogan button
     {
+        demographicToChange = 2;
+        DayController.approvalPercentageDemographics[demographicToChange] += -2;
         RemoveAllSlogans();
         sloganOpt1 = true;
     }
     public void slogan2()
     {
+        demographicToChange = 0;
+        DayController.approvalPercentageDemographics[demographicToChange] += -2;
         RemoveAllSlogans();
         sloganOpt2 = true;
     }
     public void slogan3()
     {
+        demographicToChange = 6;
+        DayController.approvalPercentageDemographics[demographicToChange] += -2;
         RemoveAllSlogans();
         sloganOpt3 = true;
     }
     public void slogan4()
     {
+        demographicToChange = 4;
+        DayController.approvalPercentageDemographics[demographicToChange] += -2;
         RemoveAllSlogans();
         sloganOpt4 = true;
     }
     public void slogan5()
     {
+        demographicToChange = 5;
+        DayController.approvalPercentageDemographics[demographicToChange] += -2;
         RemoveAllSlogans();
         sloganOpt5 = true;
     }
 
     public void Motif1() // motif unfinshed button
     {
+        demographicToChange = 0;
+        DayController.approvalPercentageDemographics[demographicToChange] += 1;
         RemoveAllMotifs();
         motifopt1 = true;
     }
     public void Motif2()
     {
+        demographicToChange = 5;
+        DayController.approvalPercentageDemographics[demographicToChange] += 1;
         RemoveAllMotifs();
         motifopt2 = true;
     }
     public void Motif3()
     {
+        demographicToChange = 1;
+        DayController.approvalPercentageDemographics[demographicToChange] += 1;
         RemoveAllMotifs();
         motifopt3 = true;
     }
     public void Motif4()
     {
+        demographicToChange = 1;
+        DayController.approvalPercentageDemographics[demographicToChange] += 1;
         RemoveAllMotifs();
         motifopt4 = true;
     }
 
     public void RemoveAllColors() // reset colors
     {
+        DayController.approvalPercentageDemographics[demographicToChange] += -2;
         blue = false;
         orange = false;
         yellow = false;
@@ -123,6 +156,7 @@ public class PosterManager : MonoBehaviour
     }
     public void RemoveAllSlogans() //reset slogans
     {
+        DayController.approvalPercentageDemographics[demographicToChange] += 2;
         sloganOpt1 = false;
         sloganOpt2 = false;
         sloganOpt3 = false;
@@ -132,6 +166,7 @@ public class PosterManager : MonoBehaviour
 
     public void RemoveAllMotifs() //reset motifs
     {
+        DayController.approvalPercentageDemographics[demographicToChange] += -1;
         motifopt1 = false;
         motifopt2 = false;
         motifopt3 = false;
