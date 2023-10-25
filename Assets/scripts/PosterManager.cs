f479e5fc8493aa6962a6e882e80725a590129f2e
ayan masud <460146@bsd48.org>
2023-10-25 14:10:06 -0700

    poster changes numbers

--- Assets/scripts/PosterManager.cs
+++ Assets/scripts/PosterManager.cs
@@ -4,25 +4,23 @@ using UnityEngine;
 
 public class PosterManager : MonoBehaviour
 {
-    public static bool blue = false;
+    public static bool blue = true;
     public static bool orange = false;
     public static bool yellow = false;
     public static bool red = false;
     public static bool green = false;
     public static bool gray = false;
 
-    public static bool sloganOpt1 = false;
+    public static bool sloganOpt1 = true;
     public static bool sloganOpt2 = false;
     public static bool sloganOpt3 = false;
     public static bool sloganOpt4 = false;
     public static bool sloganOpt5 = false;
 
-    public static bool motifopt1 = false;
+    public static bool motifopt1 = true;
     public static bool motifopt2 = false;
     public static bool motifopt3 = false;
     public static bool motifopt4 = false;
-
-    public int demographicToChange;
 
     // Start is called before the first frame update
     void Start()
@@ -38,115 +36,84 @@ public class PosterManager : MonoBehaviour
 
     public void ColorBlue() // colors of poster for button
     {
-        demographicToChange = 0;
-        DayController.approvalPercentageDemographics[demographicToChange] += 2;
         RemoveAllColors();
         blue = true;
     }
     public void ColorOrange()
     {
-        demographicToChange = 2;
-        DayController.approvalPercentageDemographics[demographicToChange] += 2;
         RemoveAllColors();
         orange = true;
     }
     public void ColorYellow()
     {
-        demographicToChange = 4;
-        DayController.approvalPercentageDemographics[demographicToChange] += 2;
         RemoveAllColors();
         yellow = true;
     }
     public void ColorRed()
     {
-        demographicToChange = 5;
-        DayController.approvalPercentageDemographics[demographicToChange] += 2;
         RemoveAllColors();
         red = true;
     }
     public void ColorGreen()
     {
-        demographicToChange = 1;
-        DayController.approvalPercentageDemographics[demographicToChange] += 2;
         RemoveAllColors();
         green = true;
     }
     public void ColorGray()
     {
-        demographicToChange = 2;
-        DayController.approvalPercentageDemographics[demographicToChange] += 2;
         RemoveAllColors();
         gray = true;
     }
 
     public void slogan1() // slogan button
     {
-        demographicToChange = 2;
-        DayController.approvalPercentageDemographics[demographicToChange] += -2;
         RemoveAllSlogans();
         sloganOpt1 = true;
     }
     public void slogan2()
     {
-        demographicToChange = 0;
-        DayController.approvalPercentageDemographics[demographicToChange] += -2;
         RemoveAllSlogans();
         sloganOpt2 = true;
     }
     public void slogan3()
     {
-        demographicToChange = 6;
-        DayController.approvalPercentageDemographics[demographicToChange] += -2;
         RemoveAllSlogans();
         sloganOpt3 = true;
     }
     public void slogan4()
     {
-        demographicToChange = 4;
-        DayController.approvalPercentageDemographics[demographicToChange] += -2;
         RemoveAllSlogans();
         sloganOpt4 = true;
     }
     public void slogan5()
     {
-        demographicToChange = 5;
-        DayController.approvalPercentageDemographics[demographicToChange] += -2;
         RemoveAllSlogans();
         sloganOpt5 = true;
     }
 
     public void Motif1() // motif unfinshed button
     {
-        demographicToChange = 0;
-        DayController.approvalPercentageDemographics[demographicToChange] += 1;
         RemoveAllMotifs();
         motifopt1 = true;
     }
     public void Motif2()
     {
-        demographicToChange = 5;
-        DayController.approvalPercentageDemographics[demographicToChange] += 1;
         RemoveAllMotifs();
         motifopt2 = true;
     }
     public void Motif3()
     {
-        demographicToChange = 1;
-        DayController.approvalPercentageDemographics[demographicToChange] += 1;
         RemoveAllMotifs();
         motifopt3 = true;
     }
     public void Motif4()
     {
-        demographicToChange = 1;
-        DayController.approvalPercentageDemographics[demographicToChange] += 1;
         RemoveAllMotifs();
         motifopt4 = true;
     }
 
     public void RemoveAllColors() // reset colors
     {
-        DayController.approvalPercentageDemographics[demographicToChange] += -2;
         blue = false;
         orange = false;
         yellow = false;
@@ -156,7 +123,6 @@ public class PosterManager : MonoBehaviour
     }
     public void RemoveAllSlogans() //reset slogans
     {
-        DayController.approvalPercentageDemographics[demographicToChange] += 2;
         sloganOpt1 = false;
         sloganOpt2 = false;
         sloganOpt3 = false;
@@ -166,7 +132,6 @@ public class PosterManager : MonoBehaviour
 
     public void RemoveAllMotifs() //reset motifs
     {
-        DayController.approvalPercentageDemographics[demographicToChange] += -1;
         motifopt1 = false;
         motifopt2 = false;
         motifopt3 = false;
