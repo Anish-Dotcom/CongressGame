cdcd7efa2ac38097b84c826cbf512765843d4737
ayanmas <460146@bsd48.org>
2023-10-26 21:51:45 -0700

    improved the transition

---
+++ Assets/transitionMainMenu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transitionMainMenu : MonoBehaviour
+{
+    private static bool created = false;
+
+    private void Awake()
+    {
+        if (!created)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            created = true;
+        }
+        else
+        {
+            // If another instance of the object exists, destroy it.
+            Destroy(this.gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
